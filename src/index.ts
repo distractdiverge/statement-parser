132a24f85cf282dda734e2e4e84a80e5987404eb
Alex Lapinski <alex.lapinski@gmail.com>
2024-03-18 16:40:45 -0400

    Removed 'pdf2image'; that project seems to not work, and last update was in 2019

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,55 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import pdf from 'pdf-parse';
+//import * as Tesseract from 'tesseract.js';
 
-function main() {
-    const filepath = path.join('data', 'sample.pdf');
 
-    let dataBuffer = fs.readFileSync(filepath);
-    pdf(dataBuffer).then(function(data) {
-        console.log(`NumPages: ${data.numpages}`);
+async function convertPDFToImages(filepath: string): Promise<string[]> {
+    let imagePaths: string[] = [];
+    
+    const options: any = {
+        outputType: 'jpg',
+    };
 
-        console.log(`Text: ${data.text}`);
-    });
+    console.info(`converting file ${filepath}`);
+
+    // TODO
+    
+    imagePaths = imagePaths.concat([]);
+    
+    return imagePaths;
+}
+
+async function extractTextFromImage(imagePath:string){}
+
+
+
+
+async function main() {
+    const filepath = path.join('.', 'data', 'sample.pdf');
+
+    let imagePaths;
+    
+    try {
+        imagePaths = await convertPDFToImages(filepath);
+    } catch (err) {
+        console.error(err);
+    }
+
+    console.log('success!');
+    console.log(imagePaths);
+
+    //let dataBuffer = fs.readFileSync(filepath);
+    //const data = await pdf(dataBuffer)
+    
+    //console.log(`NumPages: ${data.numpages}`);
+
+    //console.log(`Text: ${data.text}`);
 }
 
 
-main();
+try {
+    main();
+} catch( err ) {
+    console.error(err);
+}
